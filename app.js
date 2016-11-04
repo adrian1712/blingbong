3a123fbb9f887a28241fbd7b66b49c25552d87f1
Adrian Amaya <aa17.1atq@gmail.com>
2016-11-03 20:59:11 -0700

    add blingbong

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 function Calculate() {
-    var year = document.getElementById("leapyear").value;
-    document.getElementById("answer").innerText = LeapYearFinder(year);
-}
-function LeapYearFinder(year) {
-    if (parseInt(year) % 400 === 0)
-        return "Its a Leap Year";
-    if (parseInt(year) % 100 === 0)
-        return "Its not Leap Year";
-    if (parseInt(year) % 4 === 0)
-        return "Its a Leap Year";
-    return "Its not Leap Year";
+    var input = document.getElementById("maxNum");
+    var maxNum = parseInt(input.value);
+    var resultsText = "";
+    var listItem = "";
+    for (var i = 1; i <= maxNum; i++) {
+        if (0 == i % 3 && 0 == i % 5) {
+            listItem = "blingbong";
+        }
+        else if (0 == i % 5) {
+            listItem = "bong";
+        }
+        else if (0 == i % 3) {
+            listItem = "bling";
+        }
+        else {
+            listItem = String(i);
+        }
+        resultsText = resultsText + "<li>" + listItem + "</li>";
+    }
+    document.getElementById("results").innerHTML = resultsText;
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,35 @@
+// function Calculate(){
+// let year = document.getElementById("leapyear");
+// document.getElementById("answer").innerText = LeapYearFinder(year);
+// }
+// function LeapYearFinder(year) {
+//   if (parseInt(year) % 3 === 0)
+//   return "no";
+//   if (parseInt(year) % 5 === 0)
+//   return "need list";
+//   return "Stuff"
+// }
 function Calculate(){
-let year = document.getElementById("leapyear").value;
-document.getElementById("answer").innerText = LeapYearFinder(year);
-}
+  let input = <HTMLInputElement>document.getElementById("maxNum");
+  let maxNum = parseInt(input.value);
+  let resultsText = "";
+  let listItem = "";
 
-function LeapYearFinder(year) {
-  if (parseInt(year) % 400 === 0)
-  return "Its a Leap Year";
-  if (parseInt(year) % 100 === 0)
-  return "Its not Leap Year";
-  if (parseInt(year) % 4 === 0)
-  return "Its a Leap Year";
-  return "Its not Leap Year"
+  for(let i = 1; i <= maxNum; i++){
+    if(0 == i % 3 && 0 == i % 5){
+      listItem = "blingbong";
+    }
+    else if(0 == i % 5){
+      listItem = "bong";
+
+    }
+    else if(0 == i % 3){
+      listItem = "bling";
+    }
+    else{
+      listItem = String(i);
+    }
+    resultsText = resultsText + "<li>" + listItem + "</li>"
+  }
+  document.getElementById("results").innerHTML = resultsText;
 }
